8d6702a965c5e32de56c912d4cd6169a819550ae
Codedrin <aldrinrosales428@gmail.com>
2024-06-03 10:42:08 +0800

    fixed issue

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey:            import.meta.env.VITE_FB_API_KEY,
-    authDomain:        import.meta.env.VITE_FB_AUTH_DOMAIN,
-    databaseURL:       import.meta.env.VITE_FB_DB_URL,
-    projectId:         import.meta.env.VITE_FB_PROJ_ID,
-    storageBucket:     import.meta.env.VITE_FB_STRG_BCKT,
+    apiKey: import.meta.env.VITE_FB_API_KEY,
+    authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FB_DB_URL,
+    projectId: import.meta.env.VITE_FB_PROJ_ID,
+    storageBucket: import.meta.env.VITE_FB_STRG_BCKT,
     messagingSenderId: import.meta.env.VITE_FB_MSG_SNDR_ID,
-    appId:             import.meta.env.VITE_FB_APP_ID,
-    measurementId:     import.meta.env.VITE_FB_MSRMNT_ID
+    appId: import.meta.env.VITE_FB_APP_ID,
+    measurementId: import.meta.env.VITE_FB_MSRMNT_ID
 };
 
 const app = initializeApp(firebaseConfig);
