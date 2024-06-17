ca1dd9ada8300d16672396f69ca6c0621c0546e5
Codedrin <aldrinrosales428@gmail.com>
2024-06-17 15:20:47 +0800

    the blutooth upload is able to save on firebase

--- src/view/pages/BluetoothUpload.jsx
+++ src/view/pages/BluetoothUpload.jsx
@@ -7,7 +7,7 @@ import { LS_SETTINGS } from '../../utils/constants';
 import LOGO from '../../assets/logo.png';
 import DROPBOX_LOGO from '../../assets/images/dropbox-logo.png';
 import FIREBASE_LOGO from '../../assets/images/firebase-logo.png';
-
+import { addFile } from '../../controller/FirebaseAPI';
 
 const BluetoothUpload = () => {
     const [settings, setSettings] = useState(null);
@@ -48,7 +48,9 @@ const BluetoothUpload = () => {
             setSelectedFile(blob);
             setFileType(file.type.split('/')[1]);
 
-            console.log('File selected:', file.name, fileType);
+            // Upload the file to Firebase
+            const downloadURL = await addFile({ fileName: file.name, fileContent: blob });
+            console.log('File uploaded to Firebase with URL:', downloadURL);
         } catch (error) {
             console.error('Error loading file: ', error);
         }
@@ -57,12 +59,11 @@ const BluetoothUpload = () => {
     const handlePrintBtn = (e) => {
         navigate("/view", {
             state: {
-                bt_file   : selectedFile,
-                file_name : selectedFileName
+                bt_file: selectedFile,
+                file_name: selectedFileName
             }
         })
     }
-
 
     return (
         <main className="h-screen w-screen flex flex-col">
